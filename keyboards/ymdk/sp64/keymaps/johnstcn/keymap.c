a439f7969c3e08b20ba0b5a38d51981bbbcb853e
Cian Johnston <public@cianjohnston.ie>
2022-06-27 18:28:30 +0100

    nav is a bit too sticky for space

--- keyboards/ymdk/sp64/keymaps/johnstcn/keymap.c
+++ keyboards/ymdk/sp64/keymaps/johnstcn/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,      KC_LBRACKET, KC_RBRACKET,   KC_BSLS,
   KC_LCTL, KC_A,      KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCOLON, KC_QUOTE,                  KC_ENTER,
   KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_GRV,     KC_UP,        OSL(_MSC),
-  KC_MEH,  TT(_MSC),  KC_LALT, KC_LGUI, LT(_NAV, KC_SPACE), LT(_NAV, KC_SPACE), KC_RGUI, KC_RALT,          KC_LEFT,     KC_DOWN,      KC_RIGHT
+  KC_MEH,  TT(_MSC),  KC_LALT, KC_LGUI, LT(_NAV, KC_SPACE),          KC_SPACE,         KC_RGUI, KC_RALT,   KC_LEFT,     KC_DOWN,      KC_RIGHT
   ),
 /* Keymap 1: Navigation
  *
