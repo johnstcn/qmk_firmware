005b7f478e356b4f40f5f43557243868dd7e2d37
Cian Johnston <public@cianjohnston.ie>
2024-08-04 14:06:10 +0100

    fix keycodes after rebase

--- keyboards/ymdk/sp64/keymaps/johnstcn/keymap.c
+++ keyboards/ymdk/sp64/keymaps/johnstcn/keymap.c
@@ -1,12 +1,12 @@
 #include QMK_KEYBOARD_H
 enum layer_names {
   BASE, // default layer
-  _NAV, // navigation and function keys
-  _MSC, // misc plus numpad
+  _FN,  // function layer
+  _FX, // media keys
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
+/* Keymap 0: Basic layer.
  *
  * ┌-----┬-----┬-----┬-----┬-----┬-----┬-----┐    ┌-----┬-----┬-----┬-----┬-----┬-----┬----------┐
  * │ Esc │  1  │  2  │  3  │  4  │  5  │  6  │    │  7  │  8  │  9  │  0  │  -  │  =  │ BkSp     │
@@ -15,19 +15,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├--------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐   └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴-------┤
  * │ Ctrl    │  A  │  S  │  D  │  F  │  G  │    │  H  │  J  │  K  │  L  │  ;  │  '  │ Enter      │
  * ├---------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐   └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬-----┬-----┤
- * │ LShift   │  Z  │  X  │  C  │  V  │  B  │    │  N  │  M  │  <  │  >  │  ?  │ Sft │ Up  │_MSC │
+ * │ LShift   │  Z  │  X  │  C  │  V  │  B  │    │  N  │  M  │  <  │  >  │  ?  │  `  │ Up  │ _FX │
  * ├------┬---┴-┬---┴--┬--┴-----┴----┬┴----┬┘   ┌┴-----┴-----┴--┬--┴----┬┴-----┼-----┼-----┼-----┤
- * │ Meh  │_MSC │ Alt  │ Cmd         │_NAV │    │ _NAV / Space  │ Cmd   │ Alt  │ Lft │ Dwn │ Rgt │
+ * │ Ctrl │ Win │ Alt  │ Space       │ _FN │    │  Space        │ Cmd   │ Alt  │ Lft │ Dwn │ Rgt │
  * └------┴-----┴------┴-------------┴-----┘    └---------------┴-------┴------┴-----┴-----┴-----┘
  */
 [BASE] = LAYOUT(
-  KC_ESC,  KC_1,      KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,      KC_MINUS,    KC_EQUAL,    KC_BSPACE,
-  KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,      KC_LBRACKET, KC_RBRACKET,   KC_BSLS,
-  KC_LCTL, KC_A,      KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCOLON, KC_QUOTE,                  KC_ENTER,
-  KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_GRV,     KC_UP,        OSL(_MSC),
-  KC_MEH,  TT(_MSC),  KC_LALT, KC_LGUI, LT(_NAV, KC_SPACE),          KC_SPACE,         KC_RGUI, KC_RALT,   KC_LEFT,     KC_DOWN,      KC_RIGHT
+  KC_ESC,  KC_1,      KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,      KC_MINUS,    KC_EQUAL,    KC_BSPC,
+  KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,      KC_LBRC,     KC_RBRC,     KC_BSLS,
+  KC_LCTL, KC_A,      KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_QUOTE,                 KC_ENTER,
+  KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_GRV,      KC_UP,       MO(_FX),
+  KC_LCTL, KC_LGUI,   KC_LALT, KC_SPACE,         MO(_FN),  KC_SPACE,                   KC_RGUI, KC_RALT,   KC_LEFT,     KC_DOWN,     KC_RIGHT
   ),
-/* Keymap 1: Navigation
+/* Keymap 1: Function layer and navigation keys.
  *
  * ┌-----┬-----┬-----┬-----┬-----┬-----┬-----┐    ┌-----┬-----┬-----┬-----┬-----┬-----┬----------┐
  * │     │ F1  │  F2 │ F3  │ F4  │ F5  │ F6  │    │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │          │
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * │      │     │      │             │     │    │               │       │      │     │     │     │
  * └------┴-----┴------┴-------------┴-----┘    └---------------┴-------┴------┴-----┴-----┴-----┘
  */
-[_NAV] = LAYOUT(
+[_FN] = LAYOUT(
   _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,              KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _______,
   _______, _______, _______, KC_END,  _______, _______,           _______, KC_PGUP, _______, _______, _______, _______, _______, _______,
   _______, KC_HOME, _______, KC_PGDN, _______, _______,           KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______,
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * │      │     │      │             │     │    │               │       │      │ RGB-│ EFF-│ RGBT│
  * └------┴-----┴------┴-------------┴-----┘    └---------------┴-------┴------┴-----┴-----┴-----┘
  */
-[_MSC] = LAYOUT(
+[_FX] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______,           KC_P7,   KC_P8,   KC_P9,   _______, KC_VOLD,  KC_VOLU, KC_MUTE,
   _______, _______, _______, _______, _______, _______,           _______, KC_P4,   KC_P5,   KC_P6,   _______, KC_MPRV,  KC_MNXT, KC_MPLY,
   _______, _______, _______, _______, _______, _______,           _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,           _______,
